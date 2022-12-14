da63f42fda1f60aa11bf44b77bc9bed2384e05da
koka <nikolay.zapolskikh@gmail.com>
2022-12-14 15:30:14 +0300

    Sprint 7. Final project. Repaired.

--- single-linked-list/main.cpp
+++ single-linked-list/main.cpp
@@ -1,203 +1,150 @@
-#include "simple_vector.h"
-// Tests
-#include "tests.h"
+#include "single-linked-list.h"
 
-#include <algorithm>
 #include <cassert>
-#include <iostream>
-#include <numeric>
 
-using namespace std;
-
-class X {
-public:
-	X()
-	: X(5) {
-	}
-	X(size_t num)
-	: x_(num) {
-	}
-	X(const X& other) = delete;
-	X& operator=(const X& other) = delete;
-	X(X&& other) {
-		x_ = exchange(other.x_, 0);
-	}
-	X& operator=(X&& other) {
-		x_ = exchange(other.x_, 0);
-		return *this;
-	}
-	size_t GetX() const {
-		return x_;
+// Эта функция проверяет работу класса SingleLinkedList
+void Test() {
+	struct DeletionSpy {
+		~DeletionSpy() {
+			if (deletion_counter_ptr) {
+				++(*deletion_counter_ptr);
+			}
+		}
+		int* deletion_counter_ptr = nullptr;
+	};
+	
+	// Проверка PopFront
+	{
+		SingleLinkedList<int> numbers{3, 14, 15, 92, 6};
+		numbers.PopFront();
+		assert((numbers == SingleLinkedList<int>{14, 15, 92, 6}));
+		
+		SingleLinkedList<DeletionSpy> list;
+		list.PushFront(DeletionSpy{});
+		int deletion_counter = 0;
+		list.begin()->deletion_counter_ptr = &deletion_counter;
+		assert(deletion_counter == 0);
+		list.PopFront();
+		assert(deletion_counter == 1);
 	}
 	
-private:
-	size_t x_;
-};
-
-SimpleVector<int> GenerateVector(size_t size) {
-	SimpleVector<int> v(size);
-	iota(v.begin(), v.end(), 1);
-	return v;
-}
-
-void TestTemporaryObjConstructor() {
-	const size_t size = 1000000;
-	cout << "Test with temporary object, copy elision" << endl;
-	SimpleVector<int> moved_vector(GenerateVector(size));
-	assert(moved_vector.GetSize() == size);
-	cout << "Done!" << endl << endl;
-}
-
-void TestTemporaryObjOperator() {
-	const size_t size = 1000000;
-	cout << "Test with temporary object, operator=" << endl;
-	SimpleVector<int> moved_vector;
-	assert(moved_vector.GetSize() == 0);
-	moved_vector = GenerateVector(size);
-	assert(moved_vector.GetSize() == size);
-	cout << "Done!" << endl << endl;
-}
-
-void TestNamedMoveConstructor() {
-	const size_t size = 1000000;
-	cout << "Test with named object, move constructor" << endl;
-	SimpleVector<int> vector_to_move(GenerateVector(size));
-	assert(vector_to_move.GetSize() == size);
-	
-	SimpleVector<int> moved_vector(move(vector_to_move));
-	assert(moved_vector.GetSize() == size);
-	assert(vector_to_move.GetSize() == 0);
-	cout << "Done!" << endl << endl;
-}
-
-void TestNamedMoveOperator() {
-	const size_t size = 1000000;
-	cout << "Test with named object, operator=" << endl;
-	SimpleVector<int> vector_to_move(GenerateVector(size));
-	assert(vector_to_move.GetSize() == size);
-	
-	SimpleVector<int> moved_vector = move(vector_to_move);
-	assert(moved_vector.GetSize() == size);
-	assert(vector_to_move.GetSize() == 0);
-	cout << "Done!" << endl << endl;
-}
-
-void TestNoncopiableMoveConstructor() {
-	const size_t size = 5;
-	cout << "Test noncopiable object, move constructor" << endl;
-	SimpleVector<X> vector_to_move;
-	for (size_t i = 0; i < size; ++i) {
-		vector_to_move.PushBack(X(i));
+	// Доступ к позиции, предшествующей begin
+	{
+		SingleLinkedList<int> empty_list;
+		const auto& const_empty_list = empty_list;
+		assert(empty_list.before_begin() == empty_list.cbefore_begin());
+		assert(++empty_list.before_begin() == empty_list.begin());
+		assert(++empty_list.cbefore_begin() == const_empty_list.begin());
+		
+		SingleLinkedList<int> numbers{1, 2, 3, 4};
+		const auto& const_numbers = numbers;
+		assert(numbers.before_begin() == numbers.cbefore_begin());
+		assert(++numbers.before_begin() == numbers.begin());
+		assert(++numbers.cbefore_begin() == const_numbers.begin());
 	}
 	
-	SimpleVector<X> moved_vector = move(vector_to_move);
-	assert(moved_vector.GetSize() == size);
-	assert(vector_to_move.GetSize() == 0);
-	
-	for (size_t i = 0; i < size; ++i) {
-		assert(moved_vector[i].GetX() == i);
-	}
-	cout << "Done!" << endl;
-}
-
-void TestNoncopiablePushBack() {
-	const size_t size = 5;
-	cout << "Test noncopiable push back" << endl;
-	SimpleVector<X> v;
-	for (size_t i = 0; i < size; ++i) {
-		v.PushBack(X(i));
-	}
-	
-	assert(v.GetSize() == size);
-	
-	for (size_t i = 0; i < size; ++i) {
-		assert(v[i].GetX() == i);
-	}
-	cout << "Done!" << endl;
-}
-
-void TestNoncopiableInsert() {
-	const size_t size = 5;
-	cout << "Test noncopiable insert" << endl;
-	SimpleVector<X> v;
-	for (size_t i = 0; i < size; ++i) {
-		v.PushBack(X(i));
+	// Вставка элемента после указанной позиции
+	{  // Вставка в пустой список
+		{
+			SingleLinkedList<int> lst;
+			const auto inserted_item_pos = lst.InsertAfter(lst.before_begin(), 123);
+			assert((lst == SingleLinkedList<int>{123}));
+			assert(inserted_item_pos == lst.begin());
+			assert(*inserted_item_pos == 123);
+		}
+		
+		// Вставка в непустой список
+		{
+			SingleLinkedList<int> lst{1, 2, 3};
+			auto inserted_item_pos = lst.InsertAfter(lst.before_begin(), 123);
+			
+			assert(inserted_item_pos == lst.begin());
+			assert(inserted_item_pos != lst.end());
+			assert(*inserted_item_pos == 123);
+			assert((lst == SingleLinkedList<int>{123, 1, 2, 3}));
+			
+			inserted_item_pos = lst.InsertAfter(lst.begin(), 555);
+			assert(++SingleLinkedList<int>::Iterator(lst.begin()) == inserted_item_pos);
+			assert(*inserted_item_pos == 555);
+			assert((lst == SingleLinkedList<int>{123, 555, 1, 2, 3}));
+		};
 	}
 	
-	// в начало
-	v.Insert(v.begin(), X(size + 1));
-	assert(v.GetSize() == size + 1);
-	assert(v.begin()->GetX() == size + 1);
-	// в конец
-	v.Insert(v.end(), X(size + 2));
-	assert(v.GetSize() == size + 2);
-	assert((v.end() - 1)->GetX() == size + 2);
-	// в середину
-	v.Insert(v.begin() + 3, X(size + 3));
-	assert(v.GetSize() == size + 3);
-	assert((v.begin() + 3)->GetX() == size + 3);
-	cout << "Done!" << endl << endl;
-}
-
-void TestNoncopiableErase() {
-	const size_t size = 3;
-	cout << "Test noncopiable erase" << endl;
-	SimpleVector<X> v;
-	for (size_t i = 0; i < size; ++i) {
-		v.PushBack(X(i));
+	// Вспомогательный класс, бросающий исключение после создания N-копии
+	struct ThrowOnCopy {
+		ThrowOnCopy() = default;
+		explicit ThrowOnCopy(int& copy_counter) noexcept
+		: countdown_ptr(&copy_counter) {
+		}
+		ThrowOnCopy(const ThrowOnCopy& other)
+		: countdown_ptr(other.countdown_ptr)  //
+		{
+			if (countdown_ptr) {
+				if (*countdown_ptr == 0) {
+					throw std::bad_alloc();
+				} else {
+					--(*countdown_ptr);
+				}
+			}
+		}
+		// Присваивание элементов этого типа не требуется
+		ThrowOnCopy& operator=(const ThrowOnCopy& rhs) = delete;
+		// Адрес счётчика обратного отсчёта. Если не равен nullptr, то уменьшается при каждом копировании.
+		// Как только обнулится, конструктор копирования выбросит исключение
+		int* countdown_ptr = nullptr;
+	};
+	
+	// Проверка обеспечения строгой гарантии безопасности исключений
+	{
+		bool exception_was_thrown = false;
+		for (int max_copy_counter = 10; max_copy_counter >= 0; --max_copy_counter) {
+			SingleLinkedList<ThrowOnCopy> list{ThrowOnCopy{}, ThrowOnCopy{}, ThrowOnCopy{}};
+			try {
+				int copy_counter = max_copy_counter;
+				list.InsertAfter(list.cbegin(), ThrowOnCopy(copy_counter));
+				assert(list.GetSize() == 4u);
+			} catch (const std::bad_alloc&) {
+				exception_was_thrown = true;
+				assert(list.GetSize() == 3u);
+				break;
+			}
+		}
+		assert(exception_was_thrown);
 	}
 	
-	auto it = v.Erase(v.begin());
-	assert(it->GetX() == 1);
-	cout << "Done!" << endl << endl;
+	// Удаление элементов после указанной позиции
+	{
+		{
+			SingleLinkedList<int> lst{1, 2, 3, 4};
+			const auto& const_lst = lst;
+			const auto item_after_erased = lst.EraseAfter(const_lst.cbefore_begin());
+			assert((lst == SingleLinkedList<int>{2, 3, 4}));
+			assert(item_after_erased == lst.begin());
+		}
+		{
+			SingleLinkedList<int> lst{1, 2, 3, 4};
+			const auto item_after_erased = lst.EraseAfter(lst.cbegin());
+			assert((lst == SingleLinkedList<int>{1, 3, 4}));
+			assert(item_after_erased == (++lst.begin()));
+		}
+		{
+			SingleLinkedList<int> lst{1, 2, 3, 4};
+			const auto item_after_erased = lst.EraseAfter(++(++lst.cbegin()));
+			assert((lst == SingleLinkedList<int>{1, 2, 3}));
+			assert(item_after_erased == lst.end());
+		}
+		{
+			SingleLinkedList<DeletionSpy> list{DeletionSpy{}, DeletionSpy{}, DeletionSpy{}};
+			auto after_begin = ++list.begin();
+			int deletion_counter = 0;
+			after_begin->deletion_counter_ptr = &deletion_counter;
+			assert(deletion_counter == 0u);
+			list.EraseAfter(list.cbegin());
+			assert(deletion_counter == 1u);
+		}
+	}
 }
-
-void TestResize() {
-//	const size_t size = 3;
-//	cout << "Test noncopiable resize" << endl;
-//	SimpleVector<X> v;
-//	for (size_t i = 0; i < size; ++i) {
-//		v.PushBack(X(5));
-//	}
-//	v.Resize(7);
-//
-//	assert(v.GetSize() == 7);
-//	assert(v.GetCapacity() >= v.GetSize());
-//	
-//	SimpleVector<X> v2(5);
-//	v2.Resize(8);
-//	assert(v2.GetSize() == 8);
-//	assert(v2.GetCapacity() >= v2.GetSize());
-//	
-//	cout << "Done!" << endl;
-}
-
-void TestDeleteObj() {
-	SimpleVector<X> v;
-	v.Clear();
-	assert(v.GetSize() == 0);
-	assert(v.GetCapacity() == 0);
-	assert(v.IsEmpty());
-}
-
 
 int main() {
-    Test1();
-	Test2();
-	TestReserveConstructor();
-	TestReserveMethod();
-	
-	TestTemporaryObjConstructor();
-	TestTemporaryObjOperator();
-	TestNamedMoveConstructor();
-	TestNamedMoveOperator();
-	TestNoncopiableMoveConstructor();
-	TestNoncopiablePushBack();
-	TestNoncopiableInsert();
-	TestNoncopiableErase();
-	
-	TestResize();
-	TestDeleteObj();
-	
-    return 0;
+	Test();
 }
